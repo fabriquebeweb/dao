6c8468fa62638df279c87fe8d6d871e2d0dc774a
Valentin Creuillenet <valentincreuillenet@hotmail.fr>
2021-04-19 19:12:56 +0000

    created DAO parent methods

--- DAO.js
+++ DAO.js
@@ -1,18 +1,75 @@
 require('dotenv').config({ path : './env' })
 
 class DAO {
-    constructor () {
+    constructor (dbPath) {
+        this.dbFile = dbPath;
+    }
+
+    /**
+     * Cette méthode ouvre une connection entre l'objet et la BDD
+     */
+    openDB(){
+
+    }
+
+    /**
+     * Récupère tout les éléments d'une table/collection
+     * @param {*} target La table/collection à récupérer
+     * @param {*} callback Le traitement a effectuer sur la donnée
+     */
+    getAll(target, callback){
+
+    }
+
+    /**
+     * Récupère un élément d'une table/collection par son ID
+     * @param {*} target La table/collection dans laquelle récuperer l'élément
+     * @param {*} id L'ID de l'élément a récupérer
+     * @param {*} callback Le traitement a effectuer sur l'élément
+     */
+    getById(target, id, callback){
+
+    }
+
+    /**
+     * Inssèrt un élément dans une table/collection
+     * @param {*} target La table/collection dans laquelle insérer l'élément
+     * @param {*} element L'élément a insérer
+     * @param {*} callback Traitement à effectuer sur le message d'erreur (facultatif)
+     */
+    create(target, element, callback){
+
+    }
+
+    /**
+     * Met à a jour un élément dans une table/collection
+     * @param {*} target La table/collection de l'élément a mettre à jour
+     * @param {*} element L'élément à mettre a jour
+     * @param {*} callback Traitement à effectuer sur le message d'erreur (facultatif)
+     */
+    update(target, element, callback){
+
+    }
+
+    /**
+     * Supprime un élément d'une table/collection
+     * @param {*} target La table/collection dans laquelle supprimer l'élément
+     * @param {*} id L'id de l'élément a a supprimer
+     * @param {*} callback Traitement à effectuer sur le message d'erreur (facultatif)
+     */
+    delete(target, id, callback){
+
     }
 }
 
 exports.MongoDB = class MongoDB extends DAO {
-    constructor() {
-        super()
+    constructor(dbPath) {
+        super(dbPath)
     }
 }
 
 exports.SQLite = class SQLite extends DAO {
-    constructor() {
-        super()
+    constructor(dbPath) {
+        super(dbPath)
     }
 }
