0016a23201e2a8f0a2f915c653d98bcd843a2eba
ValentinCreuillenet <bwb.creuillenet.v@fondespierre.com>
2021-04-19 12:27:12 +0000

    init DAO

--- DAO.js
+++ DAO.js
@@ -1,1 +1,19 @@
 require('dotenv').config({ path : './env' })
+
+class DAO{
+    constructor (){
+        
+    }
+}
+
+exports.MongoDB = class MongoDB extends DAO{
+    constructor(){
+        super();
+    }
+}
+
+exports.SQLite = class SQLite extends DAO{
+    constructor(){
+        super();
+    }
+}
