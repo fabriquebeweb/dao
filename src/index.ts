47fc480e1f9d5d8f40dd89026fea62d03138bc7b
atlasrw <atlasrw@icloud.com>
2021-08-03 23:19:07 +0200

    DAO Refactoring, Tests, TypeScript, Config

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { DAO, Callback, DataElement, Connector, ConnectorParams } from './lib/dao'
+import Mongo from './lib/mongo'
+import MySQL from './lib/mysql'
+import SQLite from './lib/sqlite'
+import Redis from './lib/redis'
+
+export { Mongo, MySQL, SQLite, Redis, DAO, Callback, DataElement, Connector, ConnectorParams }
